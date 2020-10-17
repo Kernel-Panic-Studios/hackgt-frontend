f9c8d5a65e615228834f031a36bc3eb4ecf1c37c
Maanit Madan <me@maanit.me>
2020-10-17 13:13:05 -0400

    Feed fixed

--- src/pages/Feed.vue
+++ src/pages/Feed.vue
@@ -3,7 +3,8 @@
     <div class="q-pa-md row justify-center">
       <div style="width: 100%; max-width: 80%">
         <q-card class="my-card q-ma-md" v-for="(p, index) in posts" :key="index">
-          <img v-if="p.file" :src="files[index]">
+          <img v-if="p.file && (p.file.split('files/')[1].split('?')[0].split('.')[1] == 'jpg' || p.file.split('files/')[1].split('?')[0].split('.')[1] == 'png')" :src="files[index]">
+          <q-btn v-else-if="p.file" color="primary" class="full-width" label="Download Document" type="a" :href="p.file" target="_blank" />
 
           <q-card-section>
             <div class="text-h6">{{ p.text }}</div>
@@ -47,7 +48,7 @@ export default {
     loadPosts() {
       this.child = this.$q.localStorage.getItem('child');
       this.family = this.$q.localStorage.getItem('family');
-      this.fileData = this.child.id + this.family.email.replace('.', '-') + Date.now().toString();
+      this.fileData = 'a' + Date.now().toString();
       axios
         .get("http://localhost:5000/feed/" + this.child.id)
         .then(response => {
@@ -55,8 +56,9 @@ export default {
           console.log(this.posts)
           this.files = []
           for (var i in this.posts) {
-            if (this.posts[i].file.length > 0) {
-              this.getFile(this.posts[i].id);
+            if (this.posts[i].file !== null && this.posts[i].file.length > 0) {
+              console.log(this.posts[i].id)
+              this.files.push(this.posts[i].file)
             } else {
               this.files.push('');
             }
@@ -71,18 +73,6 @@ export default {
           console.log("fail");
         });
     },
-    getFile(id) {
-      axios
-        .get("http://localhost:5000/download/" + id)
-        .then(response => {
-          console.log(response);
-          var b64Response = btoa(response.data);
-          this.files.push('data:image/jpg;base64,'+b64Response);
-        })
-        .catch(() => {
-          console.log("fail");
-        });
-    },
     uploaded(f) {
       this.file = this.fileData;
       console.log(this.file);
@@ -92,7 +82,7 @@ export default {
       if (this.type == 'child') {
         sender = this.child.id;
       } 
-
+      console.log(this.file)
       axios
         .post("http://localhost:5000/post", {
           child: this.child.id,
@@ -102,6 +92,7 @@ export default {
           file: this.file
         })
         .then(response => {
+          console.log(response.data)
           this.text = '';
           this.loadPosts();
         })
